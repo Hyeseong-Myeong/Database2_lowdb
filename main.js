6c9133cc172d78784830606d9c391558737a7948
Hyeseong-int <a01025819869@gmail.com>
2021-09-29 14:50:12 +0900

    CRUD

--- main.js
+++ main.js
@@ -2,3 +2,37 @@ const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('db.json');
 const db = low(adapter);
+
+db.defaults({ topic: [], author:[]}).write()
+db.get('author').push({
+    id: 1,
+    name: 'hyeseong',
+    profile: 'dev'
+}).write();
+db.get('topic').push({
+    id:1,
+    title:'lowdb',
+    description:'lowdb is ...',
+    author:1
+}).write();
+db.get('topic').push({
+    id:2,
+    title:'mySQL',
+    description:'mySQL is ...',
+    author:1
+}).write();
+
+console.log(
+    db.get('topic')
+    .find({title:'lowdb'})
+    .value()
+);
+
+db.get('topic')
+    .find({id:2})
+    .assign({title:"mySQL & mariaDB"})
+    .write();
+
+db.get('topic')
+    .remove({id:2})
+    .write();
